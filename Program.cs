4498796b4ef4e5634d3cd6a55197d12572d4906a
Silas <s.osborne@vu.city>
2022-06-04 13:38:22 +0100

    feat: Pruning List implementation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		StretchyArray<int> sa = new StretchyArray<int>(5);
+		List2<int> list = new List2<int>();
+		list.Add(10);
+		ref int item = ref list._items[0];
+
+		Console.WriteLine("Hello World!");
+	}
+}
